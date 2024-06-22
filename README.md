c3cbdee01f1f4384209bc04493d73206a404b4a9
Matthew Cohen <cohen.matthew@ufl.edu>
2024-06-22 18:27:43 -0400

    glove update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # friendathon
+
+To run glove_test.ipynb
+
+1. Download dataset from here (we are using glove.6B.zip) https://nlp.stanford.edu/projects/glove/
+2. Extract files and copy glove.6B.300d.txt into data folder
--- process_solutions.py
+++
@@ -1,9 +0,0 @@
-def process_solutins(old_path: str, new_path: str):
-    """
-    Process old NYT connections puzzles from old_path.json and save them to new_path.pickle as a dict.
-
-    :param old_path: Path to json file with puzzle data
-    :param new_path: Path to save pickled dict to
-    :return: dict of puzzles in the form of data[puzzle][level] = dict["group"/"members"]
-    """
-    pass
